8612cb27eae5ec9b5771c30594578caf9044b5eb
Palak Choithani <palakchoithani@gmail.com>
2026-08-09 08:16:55 +0530

    Time: 183 ms (50.95%), Space: 26.9 MB (43.53%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.py
@@ -0,0 +1,40 @@
+class Solution:
+    def stoneGameII(self, piles: List[int]) -> int:
+        n = len(piles)
+
+        # suffix[i] = sum of piles from i to end
+        suffix = [0] * (n + 1)
+
+        for i in range(n - 1, -1, -1):
+            suffix[i] = suffix[i + 1] + piles[i]
+
+        dp = {}
+
+        def solve(i, M):
+            if i >= n:
+                return 0
+
+            if (i, M) in dp:
+                return dp[(i, M)]
+
+            best = 0
+
+            # Take X piles, where 1 <= X <= 2*M
+            for X in range(1, 2 * M + 1):
+
+                if i + X > n:
+                    break
+
+                # If I take X piles,
+                # opponent gets the best possible result
+                opponent = solve(i + X, max(M, X))
+
+                # Total remaining stones - opponent's stones
+                current = suffix[i] - opponent
+
+                best = max(best, current)
+
+            dp[(i, M)] = best
+            return best
+
+        return solve(0, 1)
