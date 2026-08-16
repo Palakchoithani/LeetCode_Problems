0eec37631aced5ddaa7d62337ba4210b569b1359
Palak Choithani <palakchoithani@gmail.com>
2026-08-16 14:34:44 +0530

    Time: 50 ms (58.78%), Space: 30.6 MB (61.07%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def stoneGameIX(self, stones: List[int]) -> bool:
+        count = [0, 0, 0]
+
+        for x in stones:
+            count[x % 3] += 1
+
+        # Number of 0-mod-3 stones is even
+        if count[0] % 2 == 0:
+            return count[1] > 0 and count[2] > 0
+
+        # Number of 0-mod-3 stones is odd
+        return abs(count[1] - count[2]) > 2
